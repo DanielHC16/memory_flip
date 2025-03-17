c2b88aacbc7f482bdc74905b4c5b78d6dff097c2
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-18 01:48:21 +0300

    fruit vegetable images and easy import export structures added

---
+++ constants/fruits_veg_images.ts
@@ -0,0 +1,8 @@
+import tomato from "@/assets/images/fruits_vegetables/tomato.jpg"
+import potato from "@/assets/images/fruits_vegetables/potato.jpg"
+import broccoli from "@/assets/images/fruits_vegetables/broccoli.jpg"
+import mushroom from "@/assets/images/fruits_vegetables/mushroom.jpg"
+import banana from "@/assets/images/fruits_vegetables/banana.jpg"
+import carrot from "@/assets/images/fruits_vegetables/carrot.jpg"
+
+export default { tomato, potato, broccoli, banana, mushroom, carrot }
