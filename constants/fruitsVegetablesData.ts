c4e4506b7384e07526bbfee27a1affa1de36ff07
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-18 01:49:30 +0300

    cards interfaces and mock data created

---
+++ constants/fruitsVegetablesData.ts
@@ -0,0 +1,66 @@
+import fruits from "@/constants/fruits_veg_images"
+import { CardData } from "@/models/cardType"
+import { ImageSourcePropType } from "react-native"
+
+export const FRUITS_VEGETABLES_DATA: CardData[] = [
+	{
+		id: 1,
+		label: "tomato",
+		img: fruits.tomato,
+	},
+	{
+		id: 2,
+		label: "potato",
+		img: fruits.potato,
+	},
+	{
+		id: 3,
+		label: "mushroom",
+		img: fruits.mushroom,
+	},
+	{
+		id: 4,
+		label: "tomato",
+		img: fruits.tomato,
+	},
+	{
+		id: 5,
+		label: "carrot",
+		img: fruits.carrot,
+	},
+	{
+		id: 6,
+		label: "mushroom",
+		img: fruits.mushroom,
+	},
+	{
+		id: 7,
+		label: "banana",
+		img: fruits.banana,
+	},
+	{
+		id: 8,
+		label: "broccoli",
+		img: fruits.broccoli,
+	},
+	{
+		id: 9,
+		label: "banana",
+		img: fruits.banana,
+	},
+	{
+		id: 10,
+		label: "carrot",
+		img: fruits.carrot,
+	},
+	{
+		id: 11,
+		label: "broccoli",
+		img: fruits.broccoli,
+	},
+	{
+		id: 12,
+		label: "potato",
+		img: fruits.potato,
+	},
+]
