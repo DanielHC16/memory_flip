d44c85a64a274f6bef47cc443860b5dd1beca7b9
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-19 00:58:05 +0300

    images added and easy export files updated

---
+++ constants/animal_images.ts
@@ -0,0 +1,17 @@
+import monkey from "@/assets/images/animals/monkey.png"
+import elephant from "@/assets/images/animals/elephant.png"
+import lion from "@/assets/images/animals/lion.jpg"
+import dog from "@/assets/images/animals/dog.webp"
+import cow from "@/assets/images/animals/cow.jpg"
+import cat from "@/assets/images/animals/cat.jpg"
+import animals from "@/assets/images/animals/animals.jpg"
+
+export default {
+	cat,
+	cow,
+	dog,
+	animals,
+	elephant,
+	lion,
+	monkey,
+}
--- constants/fruits_veg_images.ts
+++ constants/fruits_veg_images.ts
@@ -4,5 +4,14 @@ import broccoli from "@/assets/images/fruits_vegetables/broccoli.jpg"
 import mushroom from "@/assets/images/fruits_vegetables/mushroom.jpg"
 import banana from "@/assets/images/fruits_vegetables/banana.jpg"
 import carrot from "@/assets/images/fruits_vegetables/carrot.jpg"
+import fruit_vegs from "@/assets/images/fruits_vegetables/fruit_vegs.jpg"
 
-export default { tomato, potato, broccoli, banana, mushroom, carrot }
+export default {
+	fruit_vegs,
+	tomato,
+	potato,
+	broccoli,
+	banana,
+	mushroom,
+	carrot,
+}
