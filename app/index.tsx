3a74449cf09973b311b640cf606399d201d2ffe5
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-19 19:22:20 +0300

    main screen: text updated, status bar implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,26 @@
-import { View, Text, SafeAreaView, Image, Pressable } from "react-native"
+import { View, Text, Pressable } from "react-native"
 import React from "react"
 import "../global.css"
-import fruits from "@/constants/fruits_veg_images"
-import { Link, useRouter } from "expo-router"
-import CardProvider from "@/context/cardContext"
+import { useRouter } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import { LinearGradient } from "expo-linear-gradient"
 
 const App = () => {
 	const router = useRouter()
 	return (
-		<SafeAreaView className="flex-1">
+		<View className="flex-1">
 			<LinearGradient
 				colors={["#35E89c", "#060433"]}
 				className="justify-center flex-1 items-center"
 			>
 				<Pressable onPress={() => router.push("/screens/selection")}>
 					<Text className="text-3xl font-medium text-white">
-						GET STARTED
+						PLAY
 					</Text>
 				</Pressable>
 			</LinearGradient>
-		</SafeAreaView>
+			<StatusBar style="dark" hideTransitionAnimation="fade" />
+		</View>
 	)
 }
 
