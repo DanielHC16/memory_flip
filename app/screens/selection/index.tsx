58d27ed69e823bbf22ff7adfbadd8017c6ff09e0
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-19 19:23:52 +0300

    selection loading modal and animated gif added

--- app/screens/selection/index.tsx
+++ app/screens/selection/index.tsx
@@ -1,24 +1,47 @@
-import { View, Text, Image, Pressable, ImageBackground } from "react-native"
-import React, { useContext } from "react"
+import {
+	View,
+	Text,
+	Pressable,
+	ImageBackground,
+	Modal,
+	StyleSheet,
+} from "react-native"
+import React, { useContext, useState } from "react"
 import { LinearGradient } from "expo-linear-gradient"
 import { CATEGORIES_DATA } from "@/constants/categoryData"
 import { CardContext } from "@/context/cardContext"
 import { CardData } from "@/models/cardType"
 import { useRouter } from "expo-router"
+import { Colors } from "react-native/Libraries/NewAppScreen"
+import LottieView from "lottie-react-native"
 
 const Selection = () => {
 	const { setActiveCategoryData, setSelectedLabel, setTempLabel } =
 		useContext(CardContext)
 	const router = useRouter()
 	const handleSelection = (d: CardData[]) => {
+		setModalVisible(true)
 		setActiveCategoryData(d)
-		router.push("/cards")
+
 		setSelectedLabel("")
 		setTempLabel("")
+
+		const timeout = setTimeout(() => {
+			router.push("/cards")
+			setModalVisible(false)
+		}, 2000)
+
+		return () => clearTimeout(timeout)
 	}
+
+	const [modalVisible, setModalVisible] = useState<boolean>(false)
+
 	return (
 		<View className="flex-1">
-			<LinearGradient colors={["#35bee8", "#c545df"]} className="flex-1">
+			<LinearGradient
+				colors={["#35bee8", "#c545df"]}
+				className="flex-1 pt-8"
+			>
 				<Text className="text-white text-2xl font-bold text-center mt-4">
 					Select a category to start!
 				</Text>
@@ -49,8 +72,56 @@ const Selection = () => {
 					))}
 				</View>
 			</LinearGradient>
+			<View>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					statusBarTranslucent
+					onRequestClose={() => setModalVisible(false)}
+				>
+					<View style={styles.modalBackground}>
+						<LinearGradient
+							style={{ borderRadius: 16 }}
+							colors={[Colors.skyblue, Colors.light.tint]}
+							className="justify-center items-center py-5 w-96"
+						>
+							<Text className="text-white text-2xl font-bold">
+								Game is beginning!
+							</Text>
+							<Text className="text-white text-2xl font-bold">
+								{/* {
+								activeCategoryLabel
+							} */}
+							</Text>
+
+							<View style={styles.gifView}>
+								<LottieView
+									style={{ flex: 1 }}
+									autoPlay
+									source={require("@/assets/gifs/colordots.json")}
+									loop
+								/>
+							</View>
+						</LinearGradient>
+					</View>
+				</Modal>
+			</View>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	modalBackground: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(0, 0, 0, 0.7)",
+	},
+	gifView: {
+		height: 200,
+		aspectRatio: 1,
+	},
+})
 
 export default Selection
