9357d4e38a74590423732ea9c42fc4b8a45feda4
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-21 00:50:09 +0300

    app/index view layout and bg images changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,67 @@
-import { View, Text, Pressable } from "react-native"
-import React from "react"
+import {
+	View,
+	Text,
+	Pressable,
+	ImageBackground,
+	BackHandler,
+} from "react-native"
+import React, { useEffect, useState } from "react"
 import "../global.css"
 import { useRouter } from "expo-router"
 import { StatusBar } from "expo-status-bar"
-import { LinearGradient } from "expo-linear-gradient"
+import ExitGameModal from "@/components/modals/ExitGameModal"
 
 const App = () => {
 	const router = useRouter()
+
+	const [exitModalVisible, setExitModalVisible] = useState<boolean>(false)
+
+	useEffect(() => {
+		const backAction = () => {
+			setExitModalVisible(!exitModalVisible)
+			return true
+		}
+		BackHandler.addEventListener("hardwareBackPress", backAction)
+
+		return () => {
+			BackHandler.removeEventListener("hardwareBackPress", backAction)
+		}
+	}, [])
 	return (
 		<View className="flex-1">
-			<LinearGradient
-				colors={["#35E89c", "#060433"]}
-				className="justify-center flex-1 items-center"
+			<ImageBackground
+				source={require("@/assets/images/icon.png")}
+				resizeMode="cover"
+				style={{
+					justifyContent: "flex-end",
+					alignItems: "center",
+					flex: 1,
+				}}
 			>
-				<Pressable onPress={() => router.push("/screens/selection")}>
+				<Pressable
+					className="rounded-2xl px-4 py-2 border-2 border-white  mb-64"
+					onPress={() => router.push("/screens/selection")}
+				>
 					<Text className="text-3xl font-medium text-white">
 						PLAY
 					</Text>
 				</Pressable>
-			</LinearGradient>
-			<StatusBar style="dark" hideTransitionAnimation="fade" />
+			</ImageBackground>
+			<View>
+				<ExitGameModal
+					onClose={() => setExitModalVisible(!exitModalVisible)}
+					visible={exitModalVisible}
+					onPressNo={() => {
+						setExitModalVisible(!exitModalVisible)
+					}}
+					onPressYes={() => {
+						BackHandler.exitApp()
+						setExitModalVisible(!exitModalVisible)
+					}}
+				/>
+			</View>
+
+			<StatusBar style="light" hideTransitionAnimation="fade" />
 		</View>
 	)
 }
